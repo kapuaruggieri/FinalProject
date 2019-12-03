4a09db3fe098d5d7135d23c21e34f6665dd11669
kapuaruggieri <ruggieri@chapman.edu>
2019-12-02 20:32:32 -0800

    Edits for data

--- Lung Cancer Final.R
+++ Lung Cancer Final.R
@@ -1,5 +1,8 @@
 #Entering data is easier because there is not much and is consistant 
 #Rather than trying to run in the csv and editing and organizing it
+#Made one string of all age labels for later plots
+#Rate for black without breaking down gender, then broken down for men and women indiviudally
+#Rate for white without breaking down gender, then broken down for men and women individually
 rateblack <- c(0.0019, 0.0062, 0.0268, 0.18, 0.4648, 0.8542)
 ratemenblack <- c(0.0017, 0.006, 0.0251, 0.1753, 0.5374, 1.0165)
 ratewomenblack <- c(0.002, 0.0064, 0.0283, 0.1841, 0.407, 0.7415)
@@ -8,10 +11,16 @@ ratemenwhite <- c(0.002, 0.0062, 0.0262, 0.141, 0.4322, 0.995)
 ratewomenwhite <- c(0.0022, 0.0079, 0.0375,0.1838, 0.4509, 0.9821)
 
 #Labeling each data frame from which its coming from
+
+#All the Black and White individuals without regard to gender
 dfgenderblack <- data.frame(gender = rep(c("male","female"),each=6),age = rep(agelabels,2),rate=c(ratemenblack,ratewomenblack))
 dfgenderwhite <- data.frame(gender = rep(c("male","female"),each=6),age = rep(agelabels,2),rate=c(ratemenwhite,ratewomenwhite))
+
+#Separating the ethnic groups by gender
 dfracemen <- data.frame(race = rep(c("white","black"),each=6),age=rep(agelabels,2),rate=c(ratemenwhite,ratemenblack))
 dfracewomen <- data.frame(race = rep(c("white","black"),each=6),age=rep(agelabels,2),rate=c(ratewomenwhite,ratewomenblack))
+
+#Didn't separate gender but contrasting between ethnic groups
 dfrace <- data.frame(race = rep(c("white","black"),each=6),age=rep(agelabels,2),rate=c(ratewhite,rateblack))
 
 #Make sure ggplot package is installed and opened
