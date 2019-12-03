649b69702558ae0516917e506fa65bbe94664585
kapuaruggieri <ruggieri@chapman.edu>
2019-12-02 20:17:52 -0800

    Adding the data entries

---
+++ Lung Cancer Final.R
@@ -0,0 +1,8 @@
+#Entering data is easier because there is not much and is consistant 
+#Rather than trying to run in the csv and editing and organizing it
+rateblack <- c(0.0019, 0.0062, 0.0268, 0.18, 0.4648, 0.8542)
+ratemenblack <- c(0.0017, 0.006, 0.0251, 0.1753, 0.5374, 1.0165)
+ratewomenblack <- c(0.002, 0.0064, 0.0283, 0.1841, 0.407, 0.7415)
+ratewhite<- c(0.0021, 0.007, 0.0318, 0.1626, 0.4419, 0.9883)
+ratemenwhite <- c(0.002, 0.0062, 0.0262, 0.141, 0.4322, 0.995)
+ratewomenwhite <- c(0.0022, 0.0079, 0.0375,0.1838, 0.4509, 0.9821)
