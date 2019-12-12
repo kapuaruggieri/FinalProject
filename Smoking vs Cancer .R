b2d5c110552e1e144d0317c19ee81649e271ffaf
kapuaruggieri <ruggieri@chapman.edu>
2019-12-12 14:22:39 -0800

    Ending submissions

--- Smoking vs Cancer .R
+++ Smoking vs Cancer .R
@@ -4,3 +4,7 @@ cancer_and_smoking_data_combined <- read_csv("cancer and smoking data combined.c
 attach(cancer_and_smoking_data_combined)
 plot(smkrate, cncrrate, main = "Scatterplot of Smoking Rates vs \nLung/Bronchial Cancer Rates, by State", xlab = "Smoking Rate", ylab= "Cancer Rate", pch=19)
 abline(lm(cncrrate~smkrate), col="red")
+
+cor(cncrrate, smkrate)
+smvcr.lm = lm(cncrrate ~ smkrate, data=cancer_and_smoking_data_combined)
+summary(smvcr.lm)$r.squared
